821878a9ee7f4ca8bdff5c1054a777b318fe1e96
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-08-04 17:20:21 -0700

    Update lambda_handler.py

--- EC2/Backing-Up-EC2-Instances/Create-Backups/lambda_handler.py
+++ EC2/Backing-Up-EC2-Instances/Create-Backups/lambda_handler.py
@@ -1,35 +1,40 @@
+import boto3
 from datetime import datetime
 
-import boto3
+def create_snapshots(region):
+    ec2 = boto3.resource('ec2', region_name=region)
 
+    instances = ec2.instances.filter(
+        Filters=[
+            {'Name': 'tag:backup', 'Values': ['true']}
+        ]
+    )
+
+    timestamp = datetime.utcnow().replace(microsecond=0).isoformat()
+    snapshots_created = []
+
+    for instance in instances.all():
+        for volume in instance.volumes.all():
+            description = f'Backup of {instance.id}, volume {volume.id}, created {timestamp}'
+            print(description)
+
+            snapshot = volume.create_snapshot(Description=description)
+            snapshots_created.append(snapshot.id)
+
+    return snapshots_created
 
 def lambda_handler(event, context):
-
     ec2_client = boto3.client('ec2')
-    regions = [region['RegionName']
-               for region in ec2_client.describe_regions()['Regions']]
+    regions = [region['RegionName'] for region in ec2_client.describe_regions()['Regions']]
+
+    all_snapshots_created = []
 
     for region in regions:
+        print(f'Instances in EC2 Region {region}:')
+        snapshots_created = create_snapshots(region)
+        all_snapshots_created.extend(snapshots_created)
 
-        print('Instances in EC2 Region {0}:'.format(region))
-        ec2 = boto3.resource('ec2', region_name=region)
+    print('All snapshots created:', all_snapshots_created)
 
-        instances = ec2.instances.filter(
-            Filters=[
-                {'Name': 'tag:backup', 'Values': ['true']}
-            ]
-        )
-
-        # ISO 8601 timestamp, i.e. 2019-01-31T14:01:58
-        timestamp = datetime.utcnow().replace(microsecond=0).isoformat()
-
-        for i in instances.all():
-            for v in i.volumes.all():
-
-                desc = 'Backup of {0}, volume {1}, created {2}'.format(
-                    i.id, v.id, timestamp)
-                print(desc)
-
-                snapshot = v.create_snapshot(Description=desc)
-
-                print("Created snapshot:", snapshot.id)
+if __name__ == "__main__":
+    lambda_handler(None, None)  # For local testing if needed
